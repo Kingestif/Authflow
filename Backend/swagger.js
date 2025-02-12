006d26c1ef861626e155aafe1d74019fbe9f21d7
estif <kingestiff@gmail.com>
2025-02-12 14:35:29 +0300

    updated swagger

--- Backend/swagger.js
+++ Backend/swagger.js
@@ -28,7 +28,7 @@ const options = {
 
         servers: [
             {
-                url: "http://localhost:3000", 
+                url: "https://authflow-backend-l73i.onrender.com",  
                 description: "Local Server",
             },
         ],
@@ -40,7 +40,7 @@ const swaggerSpec = swaggerJsdoc(options);
 
 const swaggerDocs = (app) => {
     app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-    console.log("Swagger Docs available at http://localhost:3000/api-docs");
+    console.log("Swagger Docs available at https://authflow-backend-l73i.onrender.com/api-docs");
 };
 
-module.exports = swaggerDocs;
+module.exports = swaggerDocs; 
